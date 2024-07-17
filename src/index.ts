c12f07dff2f7c16739c4d090885be84e3bdf378d
Manish Patil <manishpatil1412@gmail.com>
2024-07-17 22:15:04 +0530

    git push

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 import app from './app';
 import connectDB from './db';
+const port = 8000;
 
 dotenv.config();
 
 connectDB().then(() => {
-    const PORT = process.env.PORT || 8000;
-    app.listen(PORT, () => {
-        console.log(`⚙️\t Server is running on port ${PORT}`);
-    });
+    const PORT = process.env.PORT!;
+    app.listen(port, '0.0.0.0', () => {
+        console.log(`⚙️  Server is running on port ${port}`);
+      });
 }).catch((err) => {
     console.error("Failed to connect to MongoDB:", err.message);
 });
+
