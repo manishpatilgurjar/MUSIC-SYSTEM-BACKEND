c8b927327ac49c3ac5da0d90ad503d4ebda41e2b
Manish Patil <164139396+manishpatilgurjar@users.noreply.github.com>
2024-07-18 16:29:30 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -53,10 +53,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
